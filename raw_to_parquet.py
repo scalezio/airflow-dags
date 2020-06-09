ef4569146ab0f3fc262c25a241c8063410fcc6c5
Daniel Dancziger <daniel@scalez.io>
2020-06-09 11:39:57 +0300

    moving data

--- raw_to_parquet.py
+++ raw_to_parquet.py
@@ -80,7 +80,9 @@ class S3FileTransformOperator(BaseOperator):
                 print(result.stdout)
                 print(result.stderr)
                 subprocess.Popen(["chown", "airflow:airflow", f"dags/{self.transform_script}"])
-                subprocess.Popen(["who"])
+                result = subprocess.Popen(["who"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                print(result.stdout)
+                print(result.stderr)
                 subprocess.Popen(["chmod", "775", f"dags/{self.transform_script}"])
                 result = subprocess.run(["ls", "-l", 'dags'], universal_newlines=True, stdout=subprocess.PIPE,
                                         stderr=subprocess.PIPE)
